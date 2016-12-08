d1e3270539539c481584408b11d2012fbd1f2719
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-12-09 00:53:18 +0900

    add file

---
+++ liner_search.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+"""liner search
+"""
+
+def liner_search(search_data, *data):
+    """liner search
+
+    引数:
+        search_data: 検索したい数値
+        *data: 検索する配列(list)
+
+    返り値, 戻り値:
+        return print('found it: {0}'.format(x)): 
+            検索し該当した位置をprintする
+        return print("not found: {0}".format(not_found)): 
+            該当なしの場合をprintする
+
+    例: data1 = [101, 4, 2, 22, 5]
+        liner_search(2, data1)
+        実行結果: found it: 2
+        
+
+    単体テスト: doctest:
+    >>> data1 = [101, 4, 2, 22, 5]
+    >>> liner_search(2, data1)
+    found it: 2
+
+    >>> data2 = [0, 0, 0, 0, 0]
+    >>> liner_search(2, data2)
+    not found: -1
+    """
+
+    data_list = list(*data)
+
+    not_found = -1
+    
+    try:
+       for x in data_list:
+           if x == search_data:
+               return print('found it: {0}'.format(x))
+       else:
+           return print("not found: {0}".format(not_found))
+    except (TypeError, ValueError) as err:
+        print('Error: {0}'.format(err))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    data1 = [101, 4, 2, 22, 5]
+    liner_search(2, data1)
+
+    data2 = [0, 0, 0, 0, 0]
+    liner_search(2, data2)
