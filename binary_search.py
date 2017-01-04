0dfacf7ee63437076102604a21e435d40d4e5e8d
hihumi <yui.maa5800@gmail.com>
2017-01-04 15:07:34 +0900

    add file

---
+++ binary_search.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""binary search
+"""
+
+def binary_search(search_num, *args):
+    """binary search
+
+    引数:
+        *args 探索したい数値(list)
+
+    返り値, 戻り値:
+        明示的にはなし
+
+    例:
+        data1 = [11, 13, 17, 19, 23, 29, 31]
+        binary_search(data1)
+
+    単体テスト: doctest:
+    >>> data = [11, 13, 17, 19, 23, 29, 31]
+    >>> binary_search(17, data)
+    find it: index: 2
+    """
+
+    try:
+        data_l = list(*args)
+
+        head = 0
+        tail = len(data_l) + 1
+        center = 0
+
+        while head <= tail:
+            center = (head + tail) // 2
+            if data_l[center] == search_num:
+                print('find it: index: {}'.format(data_l.index(search_num)))
+                break
+            elif data_l[center] < search_num:
+                head = center + 1
+            else:
+                tail = center - 1
+        else:
+            print('not found...')
+
+    except (TypeError, ValueError, ZeroDivisionError) as err:
+        print('Error: {0}'.format(err))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print()
+
+    data = [11, 13, 17, 19, 23, 29, 31]
+    binary_search(17, data)
