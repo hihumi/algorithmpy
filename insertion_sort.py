c9d858aacdadf278d7159343bc17744007e71a26
hihumi <yui.maa5800@gmail.com>
2017-01-09 05:11:04 +0900

    update

--- insertion_sort.py
+++ insertion_sort.py
@@ -19,12 +19,19 @@ def insertion_sort(data):
             k -= 1
 
         d_l[k] = x
-        i += 1    
+        i += 1
     print(d_l)
 
 if __name__ == '__main__':
+    import timeit
+
     data1 = [5, 3, 4, 1, 2]
     insertion_sort(data1)
+    print()
 
     data2 = [1.1, 3, 5, 2.2, 4]
     insertion_sort(data2)
+    print()
+
+    print(timeit.timeit("insertion_sort", setup="from __main__ import insertion_sort"))
+    print(timeit.repeat("insertion_sort", setup="from __main__ import insertion_sort", repeat = 3))
