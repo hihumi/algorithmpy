d3ce19ca608640a26fdfddffb00070da2ce0852c
hihumi <yui.maa5800@gmail.com>
2017-01-08 09:28:57 +0900

    add file

---
+++ bubble_sort.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""bubble sort
+"""
+
+def bubble_sort(data):
+    """bubble_sort
+    """
+
+    d_l = data
+
+    k = 0
+    while k < len(d_l) - 1:
+        i = len(d_l) - 1
+        while i > k:
+            if d_l[i - 1] > d_l[i]:
+                w = d_l[i - 1]
+                d_l[i - 1] = d_l[i]
+                d_l[i] = w
+            else:
+                i -= 1
+        k += 1
+    print(d_l)
+
+if __name__ == '__main__':
+    data1 = [5, 3, 4, 1, 2]
+    bubble_sort(data1)
+
+    data2 = [1, 3, 2, 5, 4]
+    bubble_sort(data2)
