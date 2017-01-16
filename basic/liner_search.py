62337f1db10a9c801b4e8e8e48afbbfe51cb8824
hihumi <yui.maa5800@gmail.com>
2017-01-16 17:21:41 +0900

    fix

--- basic/liner_search.py
+++ basic/liner_search.py
@@ -9,13 +9,13 @@ def liner_search(search_data, *args):
     """liner search
 
     引数:
-        search_data: 検索したい数値
-        *data: 検索する配列(list)
+        search_data: 探索したい数値
+        *data: 探索対象の配列(list)
 
     返り値, 戻り値:
-        return print('found it: {0}'.format(x)):
-            検索し該当した位置をprintする
-        return print("not found: {0}".format(not_found)):
+        return print('found it: {}'.format(x)):
+            探索し該当した数値をprintする
+        return print("not found: {}".format(not_found)):
             該当なしの場合をprintする
 
     例: data1 = [101, 4, 2, 22, 5]
@@ -42,7 +42,7 @@ def liner_search(search_data, *args):
             if x == search_data:
                 return print('found it: {}'.format(x))
         else:
-            return print("not found: {}".format(not_found))
+            return print('not found: {}'.format(not_found))
     except (TypeError, ValueError) as err:
         print('Error: {}'.format(err))
 
